93edd79a7482839f48675b9cc6bb00b9a1fa3a8d
abondar24 <abondar1992@gmail.com>
2020-11-22 22:59:15 +0100

    main fix

--- src/main/java/org/abondar/experminetal/jmsdemo/Main.java
+++ src/main/java/org/abondar/experminetal/jmsdemo/Main.java
@@ -7,6 +7,10 @@ public class Main {
 
     public static void main(String[] args) {
         CommandSwitcher switcher = new CommandSwitcher();
+        if (args.length==0){
+            System.out.println("Missing argument. Please check documentation for available argynebts");
+            System.exit(0);
+        }
         String cmd = args[0].toUpperCase();
         switcher.executeCommand(cmd);
     }
